128702b5bf77dc4d4c0b8237b65a685c5614144d
kohhorov09 <your-email@example.com>
2026-01-05 19:11:44 +0500

    edit

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -4,7 +4,7 @@ function Error() {
   const { error } = UseGetApi();
   return (
     <div className="w-full h-screen flex items-center justify-center absolute top-0 left-0 bg-white">
-      <h4 className="text-2xl text-red-500">{error}</h4>
+      <h4 className="text-2xl text-red-500">xato</h4>
     </div>
   );
 }
